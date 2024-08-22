216be968d668a4fae495d69a76cea175d36421a2
israelcruzz <israelcruz.contato@gmail.com>
2024-08-21 21:15:57 -0300

    teste 2

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get<number>('PORT') || 3333;
-  console.log(port);
-  
   await app.listen(port);
 }
 bootstrap();
