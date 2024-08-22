f2619f1a0e34382f34151e6127bd3554e1045b07
israelcruzz <israelcruz.contato@gmail.com>
2024-08-21 22:12:28 -0300

    chore: add setup for envoriment variables to prod and dev start

--- config/configuration.ts
+++
@@ -1,4 +0,0 @@
-export default () => ({
-  databaseUrl: process.env.DATABASE_URL,
-  apiKey: process.env.API_KEY,
-});
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import configuration from 'config/configuration';
 import * as path from 'path';
-
-const envPath = path.resolve(
-  __dirname,
-  '../config',
-  `.env.${process.env.NODE_ENV || 'dev'}`,
-);
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [configuration],
-      envFilePath: envPath,
-      isGlobal: true,
+      envFilePath: path.resolve(
+        __dirname,
+        '../config',
+        `.env.${process.env.NODE_ENV || 'dev'}`,
+      ),
     }),
   ],
   controllers: [],
